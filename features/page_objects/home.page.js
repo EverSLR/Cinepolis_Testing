d87a23cbf0deccb1467d35bb5ef49a24fc5657f0
Ever <everjavier95@hotmail.com>
2018-10-16 02:28:08 -0500

    match search algorithm applied

--- features/page_objects/home.page.js
+++ features/page_objects/home.page.js
@@ -1,9 +1,87 @@
 import {Page} from './page';
 
 class HomePage extends Page {
+    
+    constructor() {
+        super(); this.moviesObj = {movies : []};
+        this.matchArray = [];
+    }
+
+    //Getter
+    get city() {
+        return  browser.element('#cmbCiudades');
+    }
+
+    get form() {
+        return browser.element('#busqueda input')
+    }
+
+    get movies() {
+        return browser.elements('a[class~="datalayer-movie"]');
+    }
+
+    get moviesLink() {
+        return browser.elements('a[class~="datalayer-movie"]');
+    }
+
+    get titleSyn() {
+        return browser.getText('#ContentPlaceHolder1_ctl_sinopsis_ctl_titulo')
+    }
+
+    get theater() {
+        return browser.getText('div[idcomplejo]:not([class~="locationHide"]) h3')
+    }
+
+    get showtimesSyn() {
+        return browser.getText('div[idcomplejo]:not([class~="locationHide"]) time a')
+    }
+
+    get returnBtn() {
+        return browser.element('#ContentPlaceHolder1_link_encartelera')
+    }
+
+    //Methods
+
+    pushToObjc() {
+        let showtimes = [];
+        this.movies.value.forEach((name,i) => {
+            showtimes = browser.getText(`article:nth-child(${i + 2}) time[data-oculto="0"] a`);
+            this.moviesObj.movies.push({ 'name' : browser.elementIdText(name.ELEMENT).value, 'showtimes' : `${showtimes}` });
+        });
+    }
+
+    testMatchShowtime() {
+
+        let movieJSON = this.moviesObj.movies
+
+        for(let i = 0; i < this.moviesLink.value.length; i++) {
+            this.moviesLink.value[i].click();
+            this.returnBtn.waitForVisible();
+            //Comparacion con JSON
+            if( (movieJSON[i].name === this.titleSyn) && (movieJSON[i].showtimes === `${this.showtimesSyn}`) && (this.theater.includes('San Pedro'))) {
+                this.matchArray.push(true);
+            } else {
+                // if (movieJSON[i].name !== this.titleSyn){
+                //     console.log(`|:${movieJSON[i].name}: != :${this.titleSyn}:|`);
+                // }
+                // if (movieJSON[i].showtimes !== `${this.showtimesSyn}`) {
+                //     console.log(`|:${movieJSON[i].showtimes}: != :${this.showtimesSyn}:|`);
+                // }
+                // if (this.theater.includes('San Pedro')) {
+                //     console.log('|Includes San Pedro|');
+                // } else console.log('|Not includes San Pedro|');
+                this.matchArray.push(false);
+            }
+            this.returnBtn.click();
+        };
+    }
 
     open(max = false){
         super.open('http://www.cinepolis.com/',"", max);
+    }
+
+    removeAd() {
+        browser.click('#takeover-close img')
     }
 
     submit() {
--- features/steps/match.steps.js
+++ features/steps/match.steps.js
@@ -2,4 +2,33 @@ import homePage from '../page_objects/home.page';
 
 module.exports = function() {
 
+    this.Given(/^a client is in home page$/, function () {
+        // Write code here that turns the phrase above into concrete actions
+        homePage.open(true);
+        homePage.removeAd();
+        browser.waitUntil(function() {
+            return browser.getAttribute("#takeover","style") === "display: none;";
+          },10000);
+        // homePage.city.waitForVisible();
+      });
+
+    this.When(/^a city is selected to search Movies$/, function () {
+        // Write code here that turns the phrase above into concrete actions
+        homePage.city.selectByVisibleText('San Pedro');
+        homePage.submit();
+      });
+
+    this.Then(/^app will show all Movies' functions$/, function () {
+        // Write code here that turns the phrase above into concrete actions
+        homePage.pushToObjc();
+        console.log(homePage.moviesObj);
+        browser.pause(1000);
+      });
+
+    this.Then(/^functions must match as in the main search page$/, function () {
+        // Write code here that turns the phrase above into concrete actions
+        homePage.testMatchShowtime();
+        console.log(homePage.matchArray);
+        expect(homePage.matchArray).not.toContain(false);
+      });
 }
